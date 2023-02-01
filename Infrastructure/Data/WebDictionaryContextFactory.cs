86ac713c3a79cff2efd3ccdab0b11af3c227b546
agoloveichuk@gmail.com <agoloveichuk@gmail.com>
2023-02-01 13:19:45 +0200

    Added DbContextFactory

---
+++ Infrastructure/Data/WebDictionaryContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDictionary.Data;
+
+namespace Infrastructure.Data
+{
+    public class WebDictionaryContextFactory : IDesignTimeDbContextFactory<WebDictionaryContext>
+    {
+        public WebDictionaryContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<WebDictionaryContext>();
+            optionsBuilder.UseSqlServer();
+
+            return new WebDictionaryContext(optionsBuilder.Options);
+        }
+    }
+}
